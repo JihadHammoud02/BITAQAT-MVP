e18d916699fd177de08cbf6a0fd3e1267a5d291c
Jihad <jihadhammoud59@gmail.com>
2023-09-10 21:36:55 +0200

    fixing bugs pre Angel testing

--- Bitaqat/Bitaqat/settings.py
+++ Bitaqat/Bitaqat/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("DJANGO_SK")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'False'
 
-ALLOWED_HOSTS = ["Bitaqat-1-dev.us-west-2.elasticbeanstalk.com","127.0.0.1"]
+ALLOWED_HOSTS = ["Bitaqat-1-dev.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition

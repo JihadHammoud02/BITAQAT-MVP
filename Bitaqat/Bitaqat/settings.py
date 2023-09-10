4df264d96dbfa10a4a24228e72eac8fa31adc8ef
Jihad <jihadhammoud59@gmail.com>
2023-09-10 10:02:57 +0200

    added eb package conf

--- Bitaqat/Bitaqat/settings.py
+++ Bitaqat/Bitaqat/settings.py
@@ -115,11 +115,11 @@ if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+            'NAME': "postgres",
+            'USER': "Jihad",
+            'PASSWORD':"J279d687",
+            'HOST': "awseb-e-iqkm5t73ap-stack-awsebrdsdatabase-xrhqm9zfs5eh.chxq44fvheqy.us-west-2.rds.amazonaws.com",
+            'PORT': "5432",
         }
     }
 else:
